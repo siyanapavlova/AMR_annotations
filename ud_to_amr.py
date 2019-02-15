c88e8edcb7baab0a5d9b39e9ba402db1808db808
kelvin <kelvinhan@gmail.com>
2019-02-15 15:12:14 +0100

    .

--- ud_to_amr.py
+++ ud_to_amr.py
@@ -3,7 +3,6 @@ import grew
 def load_data(filename):
 	"""Loads a UD graph from a specified location
 	"""
-	grew.init()
 	ud_graph = grew.corpus(filename)
 	return ud_graph 
 
@@ -13,7 +12,7 @@ def ud_graph_search(ud_graph, pattern):
 	result = grew.corpus_search(pattern, ud_graph)
 	return result 
 	
-def ud_to_amr(ud_graph):
+def ud_to_amr(grs, ud_graph, strat):
 	"""Takes a UD graph in CoNNL-U format and transforms
 	it into an AMR graph by applying a set of rules
 	Parameters: ud_graph - the UD graph to be transformed; grs_filename - str, the name of the file containing the GREW string representation of a grs
@@ -24,7 +23,17 @@ def ud_to_amr(ud_graph):
 	result = grew.run(grs, ud_graph, strat)
 	return result 
 
+def __test_is_dag__(amr_graph):
+	"""Takes a ud_graph, checks if it is a directed acyclic graph and if there are 
+	returns: integer 1 if yes, integer 0 if no
 
+	"""
+	pass
+
+def __test_no_iso__(amr_graph):
+	"""Takes a ud_graph, checks that it does not have isolated components
+	returns: integer 0 if yes, integer 1 if no
+	"""
 	pass
 
 def save_data(amr_graph):
@@ -35,13 +44,18 @@ def save_data(amr_graph):
 	pass
 
 if __name__== "__main__":
+	grew.init()
+	print("Grew Initiated \n") 
+
 	# load the UD graph 
-	ud_graph = load_data("./test_LP2half.conll")
+	ud_graph = load_data("./data/dev_data/test_LP2half.conll")
+	print("Grew graph loaded \n")
+
 	# run a simple GRS 
-	grs_filename = './grs/core_roles.grs' 
-	grs = grew.grs(grs_filename)
-	print("GRS file loaded:", grs)
-	new_graph = ud_to_amr(grs_filename, ud_graph, strat="coord_gov_share")
+	grs_filename = './grs/grs_amr_main.grs' 
+
+
+	new_graph = ud_to_amr(grs_filename, ud_graph, strat="simplest")
 	# new_graph = ud_to_amr(grs_filename, ud_graph, strat="noun_amod")
 	# new_graph = ud_to_amr(grs_filename, ud_graph, strat="remove_det")
 	pass
