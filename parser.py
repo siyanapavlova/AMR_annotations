8b7f9792014a9ecdefc7cd92a42b3ed89bf80064
siyanapavlova <ad12spav@uwcad.it>
2019-02-19 16:47:32 +0100

    parser working; parsed data

--- parser.py
+++ parser.py
@@ -1,13 +1,33 @@
 from ufal.udpipe import Model, OutputFormat, Sentence, ProcessingError
+import re
+import os
 
-def load_data():
-	pass
+def load_data(file):
+	"""Loads a sentence and its ID from a text file.
+	A file is expected to contain one sentence and its ID after the sentence
+	enclosed in brackets. (e.g. "Today is a beutiful day. (001)")
 
-def parse(text):
+	Parameters: file - the path to the file to be loaded
+
+	Output: the sentence ID and the sentence itself
+	"""
+	#Load the data from a file
+	with open(file) as f:
+		sentence = f.read()
+
+	#Find the sentence_id
+	pattern = re.compile(r'\(.*\)$')
+	sentence_id = pattern.search(sentence)
+	#return the sentence_id and the sentence itself
+	return (sentence_id.group()[1:-1], sentence.replace(sentence_id.group(), ''))
+	
+
+def parse(text, sentence_id):
 	"""Takes a sentence in raw text and produces
 	its CoNLL-U annotation by invoking udpipe
 
 	Paratemeters: text - the sentence to be parsed
+				  sentence_id - the ID of the sentence
 
 	Output: a UD graph
 	"""
@@ -29,7 +49,7 @@ def parse(text):
 
 	model.parse(sentence, model.DEFAULT)
 
-	return conlluOutput.writeSentence(sentence)
+	return conlluOutput.writeSentence(sentence).replace('# sent_id = 1', '# sent_id = '+sentence_id)
 
 	# This loop would allow us to pass whole text and not just a sentence
 	# to udpipe. However, since at this stage we are working with sentences,
@@ -44,16 +64,41 @@ def parse(text):
 	# 	conllu_list.append(conlluOutput.writeSentence(sentence))
 	# return conllu_list
 
-def save_data(graph):
-	"""Takes a UD graph in CoNNL-U format and saves it into a file
+def save_data(graph, directory, filename):
+	"""Takes a string and saves it in a directory under a given name
+	Intended to be used with UD graphs in CoNNL-U format
 
 	Parameters: graph - the UD graph to be saved
+				directory - the directory where the file should be saved
+				filename - the name under which the file should be saved
 
 	Output: True or False (maybe?) or no output at all (?) or the location of the file (?)
 	"""
-	pass
+
+	#check if the directory exists and create it if it does not
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	#write the graph in a file in the provided directory
+	with open(directory+filename, 'w') as f:
+		f.write(graph)
+	return True
+
+def parse_files_in_folder(read_folder, write_folder):
+	"""Takes a folder containing .txt files with sentences,
+	parses them and saves them into another folder
+
+	Paramenters: read_folder - the folder where the original .txt files are located
+				 write_folder - the folder where the parsed sentences should be saved
+
+	Output: none
+	"""
+	for filename in os.listdir(read_folder):
+		if filename.endswith('.txt'):
+			print(filename)
+			sentence_id, sentence = load_data('./data/amr_bank_data/sentences/'+filename)
+			parsed_data = parse(sentence, sentence_id)
+			save_data(parsed_data, write_folder, filename[:-3]+'conll')
 
 if __name__== "__main__":
-	print(parse("I saw a magnificent picture."))
-	print(parse("Hi there."))
-	print(parse("Hi there. How are you?"))
+	#Parse the sentences from the amr bank (The Little Prince)
+	parse_files_in_folder('./data/amr_bank_data/sentences/', './data/amr_bank_data/ud/')
