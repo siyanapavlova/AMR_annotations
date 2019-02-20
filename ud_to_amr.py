35668c0ae6d5c151c315df2b17bb83be394e0e56
kelvinhan <kelvinhan@gmail.com>
2019-02-20 03:13:35 +0100

    save_data() and __amr_graph_to_conllu__() in ud_to_amr.py working

--- ud_to_amr.py
+++ ud_to_amr.py
@@ -123,16 +123,40 @@ def __amr_graph_to_conllu__(amr_graph):
 	Helper function. Takes an AMR in graph format, converts it into a format 
 	for convenient writing to conllu file format with the save_data function. 
 	'''
+	lines = []
+	try:
+		lines.append("#sentid = " + amr_graph["1"][0]["sentid"])
+	except: 
+		pass 
+	word_idxs = [i for i in amr_graph.keys() if i != "ROOT"]
+	word_idxs.sort()
+    
+	feature_list = ["form", "lemma", "upos", "xpos", "head", "deprel", "deps", "misc"]
+	for word_idx in word_idxs:
+		line = [word_idx]
+		for feat in feature_list: 
+			try:
+				line.append(amr_graph[word_idx][0][feat])
+			except:
+				line.append("-")
+		morph_feats = set(amr_graph[word_idx][0].keys()).difference(set(feature_list))           
+		morph_feats_list = [feat+"="+amr_graph[word_idx][0][feat]+"|" for feat in morph_feats]
+		line.insert(5,"".join(morph_feats_list))
+		line = "\t".join(line)
+		lines.append(line)
+	lines = "\n".join(lines)
+	return lines
 
-	pass 
-
-def save_data(amr_conllu):
+def save_data(amr_graph, filepath):
 	"""
 	Takes an amr graph and saves it into a file
-	input | amr_conllu: str - the AMR graph to be saved, in GREW dictionary form
-	output | str - a string representing an AMR graph in conllu format.
+	input | amr_graph: dict - the AMR graph to be saved, in GREW dictionary form
+	output | a .conllu file containing the AMR graph in conllu format.
 	"""
-	pass
+	amr_conllu = __amr_graph_to_conllu__(amr_graph)
+	with open(filepath) as file:
+		file.write(amr_conllu)
+		file.close()
 
 if __name__== "__main__":
 	grew.init()
