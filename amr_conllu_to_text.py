0d065000317c314a57eda8a466bac1c579764e32
siyanapavlova <ad12spav@uwcad.it>
2019-02-20 19:57:15 +0100

    grew to text working

--- amr_conllu_to_text.py
+++ amr_conllu_to_text.py
@@ -1,24 +1,96 @@
-import grew
+import pprint
 
+pp = pprint.PrettyPrinter(indent=4)
 
 def _load_conllu(filename):
-    g = graph
+    pass
 
+def get_text_format(graph, current_node, visited, text_format, tabs):
+	"""Takes a graph in GREW format and produces its textual representation
 
-def amr_to_conllu(graphs):
-	"""Takes a list of AMR graphs in CoNNL-U format
+	Parameters: graph - a preprocessed graph in GREW format (needs to have abbreviations
+	and a list of incoming relations for each node)
+				current_node - the current node for the recursion
+				visited - a list of already visited nodes
+				text_format - the text format so far
+				tabs - the number of tabs to be put at the beginning of each new line
+
+	Output: the visited nodes and text format of the graph so far
+	"""
+	if current_node not in visited:
+		text_format += '(' + graph[0][current_node][0]['abbreviation'] +' / ' + graph[0][current_node][0]['concept'].lower()
+		if graph[0][current_node][1] == []:
+			text_format += ')'
+			visited.append(current_node)
+		else:
+			for child_node in graph[0][current_node][1]:
+				text_format = text_format + '\n' + tabs*'\t' + ':' + child_node[0] + ' '
+				visited, text_format = get_text_format(graph, child_node[1], visited, text_format, tabs + 1)
+			text_format += ')'
+			visited.append(current_node)
+	else:
+		text_format += '(' + graph[0][current_node][0]['abbreviation'] + ')'
+	return visited, text_format
+
+def amr_grew_to_text(graph):
+	"""Takes a graph in GREW format and produces its textual representation
+	The graph is prepossed so that an abbreviation and a list of incoming
+	relations is added to each node. The recursive get_text_format function
+	is then called
+
+	Parameters: graph - a graph in GREW format
+
+	Output: the graph in text format
+	"""
+
+	#For each node add a list of incoming relations by reversing the outgoing ones
+
+	abbreviations = []
+
+	#Create abbreviations for each node
+	for node in graph[0]:
+		abbr = graph[0][node][0]['concept'][0].lower()
+		#if first letter is not in the abbreviations, the first letter becomes the abbreviation
+		if abbr not in abbreviations:
+			graph[0][node][0]['abbreviation'] = abbr
+			abbreviations.append(abbr)
+		else:
+			previous = [x for x in abbreviations if x.startswith(abbr)]
+			graph[0][node][0]['abbreviation'] = abbr + str(len(previous)+1)
+
+	#Make a list in each node where the incoming relations will be kept
+	for node in graph[0]:
+		graph[0][node].append([])
+
+	#Fill the incoming relations list
+	for node in graph[0]:
+		for relation in graph[0][node][1]:
+			graph[0][relation[1]][2] = [relation[0],node]
+
+	#Find the starting node (root)
+	for node in graph[0]:
+		if graph[0][node][2] == []:
+			starting_node = node
+			break
+
+	#Get the text format of the preprocessed graph with a starting node
+	visited, text_format = get_text_format(graph, starting_node, [], '', 1)
+	return text_format
+
+def conllu_for_smatcher(graphs):
+	"""Takes a list of AMR graphs in GREW format
 	and saves them all into a file which follows the
 	format accepted by SMATCH
 
-	Parameters: graphs - a list of AMR graphs in CoNNL-U format
+	Parameters: graphs - a list of AMR graphs in GREW format
 
 	Output: True/False? Or the location of the file?
 	"""
-    
-    
-    
-	pass
+
+	# Would need graph ID for each graph as well
+	for graph in graphs:
+		text_format = amr_grew_to_text(graph)
 
 if __name__== "__main__":
-    
-	pass
+	new_graph = [{'2': [{'xpos': 'VBD', 'upos': 'VERB', 'lemma': 'see', 'form': 'saw', 'concept': 'see-01', 'Mood': 'Ind', 'VerbForm': 'Fin', 'Tense': 'Past'}, [['lex.doer', '1'], ['lex.patient', '5']]], '5': [{'xpos': 'NN', 'upos': 'NOUN', '_MISC_SpaceAfter': 'No', 'lemma': 'picture', 'form': 'picture', 'concept': 'picture', 'Number': 'Sing'}, [['ARG0-of', '4']]], '4': [{'concept': 'magnificent', 'lemma': 'magnificent', 'form': 'magnificent', 'Degree': 'Pos', 'xpos': 'JJ', 'upos': 'ADJ'}, []], '1': [{'xpos': 'PRP', 'Person': '1', 'lemma': 'I', 'form': 'I', 'PronType': 'Prs', 'Case': 'Nom', 'Number': 'Sing', 'upos': 'PRON', 'concept': 'I'}, []]}]
+	print(amr_grew_to_text(new_graph))
