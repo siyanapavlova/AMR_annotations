5d7808d42abaa375d711fee07fcd84b699191ae6
siyanapavlova <ad12spav@uwcad.it>
2019-03-28 14:45:45 +0100

    First results out

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-import ud_to_amr, amr_graph_to_text, smatcher
+import ud_to_amr, amr_graph_to_text, smatcher, parser
 import grew
 from random import seed, sample
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 
-def run_pipeline(load_path, save_path): 
-	print(load_path)
+def run_pipeline(load_path, save_path, filename, gold_amr): 	
 	# load the UD graph 
 	ud_graph = ud_to_amr.load_data(load_path)
-	#pp.pprint(ud_graph)
+	
 	#print("Grew graph loaded \n")
-	# print(ud_graph)
 
 	# run a simple GRS 
 	grs_filename = './grs/grs_amr_main.grs' 
 
 	# generate the graph(s) from the application of the grs in grs_filename
 	new_graphs = ud_to_amr.ud_to_amr(grs_filename, ud_graph, strat="simple")
-	# print(new_graphs[0])
-	# print(len(new_graphs))
-	# ud_to_amr.save_data(new_graphs[0],save_path)
-	#pp.pprint(new_graphs[0])
-	print(amr_graph_to_text.amr_grew_to_text(new_graphs))
+	
+	text_amr = amr_graph_to_text.amr_grew_to_text(new_graphs)
+	parser.save_data(text_amr, save_path, filename)
+
+	score = smatcher.get_smatch_score(gold_amr,save_path+filename)
+	# print(save_path+filename, gold_amr)
+	return score
+
 
 if __name__=="__main__":
 	# sentence_nums = choose_sentences_from_lp(20, 1, 1562)
@@ -30,8 +31,13 @@ if __name__=="__main__":
 	print("Grew initiated \n")
 
 	seed(1)
-	# sentence_nums = sample(range(1,1562,1),20)
-	sentence_nums = [2]
+	sentence_nums = sorted(sample(range(1,1562,1),20))
+	# sentence_nums = [2]
+	scores = []
 	print(sentence_nums)
 	for num in sentence_nums:
-		run_pipeline('./data/amr_bank_data/ud/sentence'+"{:04d}".format(num)+'.conll', './data/evaluation/sentence'+"{:04d}".format(num)+'.conll')
+		scores.append(('{:04d}'.format(num), run_pipeline('./data/amr_bank_data/ud/sentence'+'{:04d}'.format(num)+'.conll',
+							'./data/evaluation/',
+							'sentence'+'{:04d}'.format(num)+'.txt',
+							'./data/amr_bank_data/amrs/amr'+'{:04d}'.format(num)+'.txt')))
+	pp.pprint(scores)
