361a7b2486b88bf3897b5f69107517f2c46fdff3
siyanapavlova <ad12spav@uwcad.it>
2019-05-27 21:00:54 +0200

    finalrules and updated main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import grew
 from random import seed, sample
 import pprint
 import operator
+import csv
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -13,9 +14,11 @@ def run_pipeline(load_path, save_path, filename, gold_amr):
 	#print("Grew graph loaded \n")
 
 	# run a simple GRS 
-	grs_filename = './grs/grs_amr_main.grs' 
+	grs_filename = './grs/grs_amr_main.grs'
 
-	amr_graph_to_text.amr_grew_to_text(ud_graph)
+	#printing out the grew format as text; for testing and analysis purposes
+	# new_graphs = ud_to_amr.ud_to_amr(grs_filename, ud_graph, strat="add_conc")
+	# print(amr_graph_to_text.amr_grew_to_text(new_graphs[0]))
 
 	# generate the graph(s) from the application of the grs in grs_filename
 	print("Currently at " + filename)
@@ -50,33 +53,7 @@ def run_pipeline(load_path, save_path, filename, gold_amr):
 	# print(save_path+filename, gold_amr)
 	return score
 
-
-if __name__=="__main__":
-	# sentence_nums = choose_sentences_from_lp(20, 1, 1562)
-	grew.init()
-	print("Grew initiated \n")
-
-	seed(1)
-	# sentence_nums = sorted(sample(range(1,1562,1),20))
-	# sentence_nums = [59, 276, 430, 523, 778, 799, 887, 1166, 1245, 1426]
-	# sentence_nums = [347]
-
-	# sentence_nums = list(range(1,121))
-	# sentence_nums = list(range(1,15))
-	sentence_nums = [32]
-	# sentence_nums = [104]
-
-	# sentence_nums = list(range(1,1563))
-	# sentence_nums.remove(347)
-	# sentence_nums.remove(406)
-	# sentence_nums.remove(662)
-	# sentence_nums.remove(697)
-	# sentence_nums.remove(818)
-	# sentence_nums.remove(1272)
-	# sentence_nums.remove(1344)
-	scores = []
-	print(sentence_nums)
-
+def calculate_scores(sentence_nums):
 	for num in sentence_nums:
 		result = run_pipeline('./data/amr_bank_data/ud/sentence'+'{:04d}'.format(num)+'.conll',
 							'./data/evaluation/',
@@ -89,6 +66,7 @@ if __name__=="__main__":
 	precision = [float(score[1].split(b"Precision: ")[1].split(b"\n")[0]) for score in scores]
 	recall = [float(score[1].split(b"Recall: ")[1].split(b"\n")[0]) for score in scores]
 	f1 = [float(score[1].split(b"F-score: ")[1].split(b"\n")[0]) for score in scores]
+
 	for score_val in [precision, recall, f1]:
 		if score_val:
 			print("======================")
@@ -100,3 +78,43 @@ if __name__=="__main__":
 			print(index, value)
 			print(score_val)
 	print(len(precision))
+	return precision, recall, f1
+
+def collect_scores(sentence_nums, n, filename):
+	'''
+	Parameters: n - the number of times that the test should be run
+				filename - path to the csv file where the results should be saved
+				sentence_nums - a list of sentence numbers to be tested
+	'''
+	with open(filename, 'w') as csvfile:
+		writefile = csv.writer(csvfile, delimiter='\t')
+		writefile.writerow(['Measure', 'Min', 'Max', 'Average', 'Data'])
+		for i in range(n):
+			precision, recall, f1 = calculate_scores(sentence_nums)
+			for score_val in [('precision', precision), ('recall', recall), ('f1',f1)]:
+				min_score = min(score_val[1])
+				max_score = max(score_val[1])
+				avg_score = sum(score_val[1])/len(score_val[1])
+				writefile.writerow([score_val[0], min_score, max_score, avg_score, score_val[1]])
+			writefile.writerow(['','','','',''])
+
+
+
+
+if __name__=="__main__":
+	# sentence_nums = choose_sentences_from_lp(20, 1, 1562)
+	grew.init()
+	print("Grew initiated \n")
+
+	seed(1)
+	# sentence_nums = sorted(sample(range(1,1562,1),20))
+	# sentence_nums = [59, 276, 430, 523, 778, 799, 887, 1166, 1245, 1426]
+	# sentence_nums = [347]
+
+	sentence_nums = list(range(1,101))
+
+	scores = []
+	print(sentence_nums)
+
+	# calculate_scores(sentence_nums)
+	collect_scores(sentence_nums, 1, './best_output.csv')
